9a58638dc1639225e926d7455f43f19a2cb24f4e
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-03-23 11:00:55 -0700

    feat: basic dragging is working, much work still needed

--- src/routes/Create/components/PedalBoardSandBox/components/Sandbox/Sandbox.tsx
+++ src/routes/Create/components/PedalBoardSandBox/components/Sandbox/Sandbox.tsx
@@ -1,8 +1,43 @@
+import { useState, MouseEvent } from 'react'
+
 const Sandbox = () => {
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  const [isDragging, setIsDragging] = useState(false)
+
+  const handleMouseDown = (event: MouseEvent) => {
+    event.preventDefault()
+    if (event.currentTarget.classList.contains('draggable')) {
+      setIsDragging(true)
+    }
+  }
+
+  const handleMouseUp = (event: MouseEvent) => {
+    event.preventDefault()
+    setIsDragging(false)
+  }
+
+  const handleMouseMove = (event: MouseEvent) => {
+    console.log(isDragging)
+    if (isDragging) {
+      setX(event.clientX)
+      setY(event.clientY)
+    }
+  }
   return (
-    <section role="sandbox" className="pedal-dashboard-grid w-full h-[50vh]">
-      <div id="draggable-box" className="w-[100px] h-[100px] bg-green-400"></div>
-    </section>
+    <section
+      role="sandbox"
+      className="pedal-dashboard-grid w-full h-[50vh]"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}>
+      <div
+        id="draggable-box"
+        className={`draggable w-[100px] h-[100px] bg-green-400`}
+        style={{ position: 'absolute', top: y, left: x }}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}>{`${x}-${y}`}</div>
+    </section >
   )
 }
 
