5b810ecee53c94db6718d8f6f15c73973cffa7f5
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-03-29 17:35:28 -0700

    feat: starting work on pedal menu list, need to support multiple versions of same pedal still

--- src/routes/Create/components/Menu/Menu.tsx
+++ src/routes/Create/components/Menu/Menu.tsx
@@ -1,19 +1,51 @@
+import { useState, useCallback, type MouseEvent } from 'react'
+
 import { usePedalStore } from '../PedalBoardSandBox/components/Sandbox/store/pedal'
-const mockPedal = {
-  id: 3,
-  name: 'bmp',
+
+const mockPedalList = [{
+  id: 1,
+  name: 'Big Muff Pi',
   brand: '-',
   img: 'bmp.png',
   location: {
     w: 192,
     h: 253,
-    x: 450,
-    y: 200
+    x: 0,
+    y: 0
   }
-}
+}, {
+  id: 2,
+  name: 'ProCo Rat',
+  brand: '-',
+  img: 'rat.png',
+  location: {
+    w: 263,
+    h: 300,
+    x: 0,
+    y: 0
+  }
+}]
+
 const Menu = () => {
 
+  const [pedalList] = useState(mockPedalList)
   const addNewPedal = usePedalStore((state) => state.addNewPedals)
+
+  const savePedalDataById = useCallback((event: MouseEvent) => {
+    event.preventDefault()
+    const elem = event.target as HTMLElement
+    const pedalId = elem && parseInt(elem.getAttribute('data-pedal-id') || '', 10)
+
+    if (!pedalId) return
+    const pedalById = pedalList.find((item) => {
+      return item.id === pedalId
+    })
+
+    if (!pedalById) return
+
+    return addNewPedal(pedalById)
+
+  }, [pedalList, addNewPedal])
 
   return (
     <section
@@ -26,10 +58,19 @@ const Menu = () => {
           <button>Boards v</button>
         </li>
         <li>
-          <button onClick={() => addNewPedal(mockPedal)}>Pedals v</button>
+          <button>Pedals v</button>
+          <ol>
+            {pedalList.map((item) => (
+              <li>
+                <button
+                  data-pedal-id={`${item.id}`}
+                  onClick={savePedalDataById}>{item.name}</button>
+              </li>
+            ))}
+          </ol>
         </li>
-      </menu>
-    </section>
+      </menu >
+    </section >
   )
 }
 
--- src/routes/Create/components/PedalBoardSandBox/components/Sandbox/store/pedal.tsx
+++ src/routes/Create/components/PedalBoardSandBox/components/Sandbox/store/pedal.tsx
@@ -1,28 +1,6 @@
 import { create } from 'zustand'
 import type { PedalShape } from '../../Pedal/Pedal.types'
 
-const mockPedalJSON = [{
-  id: 1,
-  name: 'test pedal',
-  brand: 'JRGarcia Amps',
-  location: {
-    w: 100,
-    h: 100,
-    x: 0,
-    y: 0
-  }
-}, {
-  id: 2,
-  name: 'test pedal 2',
-  brand: 'JRGarcia Amps',
-  location: {
-    w: 200,
-    h: 100,
-    x: 200,
-    y: 200
-  }
-}]
-
 interface PedalStore {
   pedals: PedalShape[];
   addNewPedals: (newPedal: PedalShape) => void;
@@ -33,17 +11,18 @@ interface PedalStore {
 }
 
 const usePedalStore = create<PedalStore>((set) => ({
-  pedals: mockPedalJSON,
+  pedals: [],
+  history: [],
+  redoLastHistory: null,
 
   addNewPedals: (newPedal) => set((state) => {
     const updatedState = [...state.pedals, newPedal]
+
     return ({
       pedals: [...updatedState],
       history: [...state.history, updatedState]
     })
   }),
-
-  history: [mockPedalJSON],
 
   updateHistory: (newPedals) => set((state) => {
     return ({
@@ -53,18 +32,17 @@ const usePedalStore = create<PedalStore>((set) => ({
   }),
 
   undoHistory: () => set((state) => {
-    if (state.history.length < 2) return state
+    if (state.history.length < 1) return state
     const updatedState = [...state.history]
-    // TODO Add redo feature with last state
+
     const lastState = updatedState.pop()
 
     return ({
-      pedals: updatedState[updatedState.length - 1],
+      pedals: updatedState[updatedState.length - 1] || [],
       history: updatedState,
       redoLastHistory: lastState
     })
   }),
-  redoLastHistory: null
 }))
 
 export { usePedalStore }
