799384a2f95e37ce7a105b2ac57c055c2da2aad3
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-06-07 13:03:48 -0700

    chore: remove leftover commented code

--- src/routes/MyGear/MyGear.tsx
+++ src/routes/MyGear/MyGear.tsx
@@ -1,72 +1,12 @@
-import { useMemo, useEffect, Fragment } from "react"
+import { Fragment } from "react"
 import { Link } from "react-router"
-import { buttonVariants } from "@/components/ui/button"
 import { useGetMyPedals } from "@/queryHooks/myGear/useGetMyPedals"
-import { PedalSelector } from "../Create/components/Menu/PedalSelector"
 import { useGetAllPedals } from "@/queryHooks/pedalBoard/useGetAllPedals"
-import { useSaveUserPedal } from "@/queryHooks/myGear/useSaveUserPedal"
-import { useDeleteUserPedal } from "@/queryHooks/myGear/useSaveUserPedal"
-import { useMyGearStore } from "./state/useMyGearStore"
 import { Separator } from "@/components/ui/separator"
 
 const MyGear = () => {
-  const { isLoading, isSuccess, isError, data } = useGetMyPedals()
-  const { isLoading: allPedalsLoading, pedalList } = useGetAllPedals()
-  const mutation = useSaveUserPedal()
-  const deleteMutation = useDeleteUserPedal()
-
-  const myPedalList = useMyGearStore((state) => state.pedalList)
-  const setPedalList2 = useMyGearStore((state) => state.setPedalList)
-  const addPedalToList = useMyGearStore((state) => state.addPedalToList)
-  const removePedalFromList = useMyGearStore((state) => state.removePedalFromList)
-
-  // const myPedalIdList = useMemo(() => {
-  //   if (!data || !data.data) return []
-  //   return data.data.map(pedal => pedal.pedal_id)
-  // }, [data])
-
-  // const handleSavepedalDataById = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //   event.preventDefault()
-  //   const elem = event.currentTarget as HTMLElement
-  //   const pedalId = elem && parseInt(elem.getAttribute('data-pedal-id') || '', 10)
-
-  //   mutation.mutate({ pedal_id: pedalId, notes: {} })
-  // }
-
-  // const handleDeletePedal = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //   event.preventDefault()
-  //   const elem = event.currentTarget as HTMLElement
-  //   const pedalId = elem && parseInt(elem.getAttribute('data-pedal-id') || '', 10)
-
-  //   deleteMutation.mutate({ pedal_id: pedalId })
-
-  // }
-
-  // useEffect(() => {
-  //   if (myPedalIdList && myPedalIdList?.length !== 0) {
-  //     setPedalList2(myPedalIdList)
-  //   }
-  // }, [myPedalIdList, setPedalList2])
-
-  // useEffect(() => {
-  //   if (mutation.isSuccess) {
-  //     addPedalToList(mutation?.data?.data?.[0]?.pedal_id)
-  //   }
-  // }, [mutation.isSuccess, mutation.data, addPedalToList])
-
-  // useEffect(() => {
-  //   if (deleteMutation.isSuccess) {
-  //     removePedalFromList(deleteMutation?.data?.data?.[0]?.pedal_id)
-  //   }
-  // }, [deleteMutation.isSuccess, deleteMutation.data, removePedalFromList])
-
-  // if (isLoading || allPedalsLoading)
-  //   return <h2>...Loading</h2>
-
-  // if (isError)
-  //   return <h2>...Error!, {JSON.stringify(data?.error || {})}</h2>
-
-  // console.log('pedalList', myPedalIdList, myPedalList)
+  const { data } = useGetMyPedals()
+  const { pedalList } = useGetAllPedals()
 
   return (
     <>
@@ -78,14 +18,6 @@ const MyGear = () => {
 
         <section className="flex flex-col">
           <h3 className="text-2xl font-[bebas_neue]">My Pedals</h3>
-          {/* <PedalSelector
-            className={`m-0 w-fit ${buttonVariants({ variant: "outline" })}`}
-            myPedalIdList={myPedalList}
-            isSuccess={isSuccess}
-            pedalList={pedalList}
-            savePedalDataById={handleSavepedalDataById}
-            deletePedalDataById={handleDeletePedal}
-          /> */}
           {data?.data && data?.data.map((userPedalId) => {
             const linkedPedal = pedalList?.find((pedal) => {
               return parseInt(userPedalId.pedal_id, 10) === pedal.id
