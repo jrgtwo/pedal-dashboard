5be068d6ff0d27ff6b0e0f51c59b58f9adc80b79
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-06-25 18:41:59 -0700

    fix: small layout update myGear page

--- src/routes/MyGear/MyGear.tsx
+++ src/routes/MyGear/MyGear.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router"
 import { useGetMyPedals } from "@/queryHooks/myGear/useGetMyPedals"
 import { Separator } from "@/components/ui/separator"
 import { AddGear } from "./AddGear"
+import { Badge } from "@/components/ui/badge"
 
 const MyGear = () => {
   const { isLoading, isSuccess, data } = useGetMyPedals()
@@ -25,15 +26,21 @@ const MyGear = () => {
             return (
               <Fragment key={pedal?.pedals?.id || pedal?.pedal_id}>
                 <Link
-                  className="flex p-8 hover:bg-gray-100 rounded-2xl gap-8"
+                  className="flex p-8 hover:bg-gray-200 rounded-2xl gap-8 even:bg-gray-100 odd:bg-white"
                   to={`/my-gear/pedals/${pedal?.id}/${pedal?.pedals?.id}/${encodeURIComponent(pedal?.pedals?.name.replace(/ /g, '-'))}`}>
                   <img width="50" src={`/src/assets/${pedal?.pedals?.img}`} />
                   <div>
-                    <span>{pedal?.pedals?.name}</span>
+                    <p>{pedal?.pedals?.name}</p>
+                    <div className="flex gap-2">
+                      {pedal?.pedals?.type.map((type, index) => {
+                        return <Badge key={type}>{type}</Badge>
+                      })}
+                    </div>
+
                   </div>
 
                 </Link>
-                <Separator />
+
               </Fragment>
             )
           })}
