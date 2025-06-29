57f1f4d796c92f93ad214f581bca108b869e40ea
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-06-29 13:06:21 -0700

    feat: small header visual update

--- src/main-components/Header/Header.tsx
+++ src/main-components/Header/Header.tsx
@@ -4,10 +4,10 @@ import { Navigation } from '../../main-components/Navigation/Navigation'
 const Header = () => {
   return (
     <header role="header"
-      className=" flex flex-row justify-between px-8 py-4 fixed w-full bg-white shadow-md z-10">
+      className="backdrop-blur-md flex flex-row justify-between px-8 py-4 fixed w-full bg-white/60 shadow-md z-100">
 
       <Link to="/">
-        <h1 className="text-5xl font-heading" >Pedal DASHBOARD</h1>
+        <h1 className="text-shadow-white text-shadow-2xl text-5xl font-heading " >Pedal DASHBOARD</h1>
         <p className="text-2-xl font-heading">Layout and save your dream pedalboard</p>
       </Link>
 
