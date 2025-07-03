053f0c7126e67437f4ff6e12c597a21c3c520856
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-07-02 19:38:24 -0700

    refactor: splitting sandbox into component and effects for easier readability for now

--- src/routes/Create/components/PedalBoardSandBox/components/Sandbox/Sandbox.tsx
+++ src/routes/Create/components/PedalBoardSandBox/components/Sandbox/Sandbox.tsx
@@ -1,62 +1,15 @@
-import { useEffect, useState, useCallback } from 'react'
-import { useParams } from 'react-router'
-import type { DraggablePedalShape } from '../Pedal/Pedal.types'
-import { usePedalStore } from './store/pedal'
 import { Pedal } from '../Pedal/Pedal'
-import { useDraggable } from './hooks/useDraggable'
 import testboard from '../../../../../../assets/test-board.png'
-import { useGetBoardById } from '../../../../../../queryHooks/pedalBoard/useGetBoardById'
+import { useSandbox } from './hooks/useSandbox'
 
 const Sandbox = () => {
-  const [hasSetInitial, setHasSetInitial] = useState(false)
-
-  const pedals = usePedalStore((state) => state.pedals)
-  const updateHistory = usePedalStore((state) => state.updateHistory)
-  const removeBy = usePedalStore((state) => state.removeBy)
-
-  const { boardId } = useParams()
-  const query = useGetBoardById(Number(boardId))
-  const updateFromFetch = usePedalStore((state) => state.updateFromFetch)
-
-  useEffect(() => {
-    if (query.isSuccess && !hasSetInitial) {
-      setHasSetInitial(true)
-      updateFromFetch({
-        id: query.data?.data?.[0].id,
-        pedals: JSON.parse(query.data?.data?.[0].board),
-        name: query.data?.data?.[0].name
-      })
-    }
-  }, [hasSetInitial, query.isSuccess, query.data, updateFromFetch])
-
   const {
-    setter,
-    onDraggingComplete,
     draggableArray,
     handleMouseDown,
     handleMouseUp,
     handleMouseMove,
-  } = useDraggable<DraggablePedalShape>(pedals)
-
-  useEffect(() => {
-    setter(pedals)
-  }, [pedals, setter])
-
-
-  useEffect(() => {
-    let ignore = false
-    onDraggingComplete((updatedDraggableArray, target) => {
-      if (target?.classList.contains('remove')) return
-      if (ignore) return
-      updateHistory(updatedDraggableArray)
-    })
-
-    return () => { ignore = true }
-  }, [onDraggingComplete, updateHistory])
-
-  const handleRemove = useCallback((key: keyof DraggablePedalShape, value: string) => {
-    removeBy(key, value)
-  }, [removeBy])
+    handleRemove
+  } = useSandbox()
 
   return (
     <section
@@ -66,7 +19,7 @@ const Sandbox = () => {
       onMouseDown={(event) => handleMouseDown(event)}
       onMouseUp={(event) => handleMouseUp(event)}
       onMouseMove={(event) => handleMouseMove(event)}>
-    <div
+      <div
         className="pedal-dashboard-grid"
       >
         <img
---
+++ src/routes/Create/components/PedalBoardSandBox/components/Sandbox/hooks/useSandbox.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState, useCallback } from 'react'
+import { useParams } from 'react-router'
+import type { DraggablePedalShape } from '../../Pedal/Pedal.types'
+import { usePedalStore } from '../store/pedal'
+import { useDraggable } from '../hooks/useDraggable'
+import { useGetBoardById } from '../../../../../../../queryHooks/pedalBoard/useGetBoardById'
+
+const useSandbox = () => {
+  const [hasSetInitial, setHasSetInitial] = useState(false)
+
+  const pedals = usePedalStore((state) => state.pedals)
+  const updateHistory = usePedalStore((state) => state.updateHistory)
+  const removeBy = usePedalStore((state) => state.removeBy)
+
+  const { boardId } = useParams()
+  const query = useGetBoardById(Number(boardId))
+  const updateFromFetch = usePedalStore((state) => state.updateFromFetch)
+
+  useEffect(() => {
+    if (query.isSuccess && !hasSetInitial) {
+      setHasSetInitial(true)
+      updateFromFetch({
+        id: query.data?.data?.[0].id,
+        pedals: JSON.parse(query.data?.data?.[0].board),
+        name: query.data?.data?.[0].name
+      })
+    }
+  }, [hasSetInitial, query.isSuccess, query.data, updateFromFetch])
+
+  const {
+    setter,
+    onDraggingComplete,
+    draggableArray,
+    handleMouseDown,
+    handleMouseUp,
+    handleMouseMove,
+  } = useDraggable<DraggablePedalShape>(pedals)
+
+  useEffect(() => {
+    setter(pedals)
+  }, [pedals, setter])
+
+
+  useEffect(() => {
+    let ignore = false
+    onDraggingComplete((updatedDraggableArray, target) => {
+      if (target?.classList.contains('remove')) return
+      if (ignore) return
+      updateHistory(updatedDraggableArray)
+    })
+
+    return () => { ignore = true }
+  }, [onDraggingComplete, updateHistory])
+
+  const handleRemove = useCallback((key: keyof DraggablePedalShape, value: string) => {
+    removeBy(key, value)
+  }, [removeBy])
+
+  return {
+    draggableArray,
+    handleMouseDown,
+    handleMouseUp,
+    handleMouseMove,
+    handleRemove
+  }
+}
+
+export { useSandbox }
