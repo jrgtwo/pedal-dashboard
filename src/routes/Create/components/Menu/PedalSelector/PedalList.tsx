8708b181616a4c14c979fe63524bf6463c902cca
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-07-02 19:28:27 -0700

    chore: ui cleanup pedal selector

--- src/routes/Create/components/Menu/PedalSelector/PedalList.tsx
+++ src/routes/Create/components/Menu/PedalSelector/PedalList.tsx
@@ -13,15 +13,15 @@ export const PedalList = ({ pedalList, savePedalDataById, deletePedalDataById, m
   return (
     <main className="overflow-scroll w-full">
       <h2>Select a Pedal</h2>
-      <section className="flex flex-wrap mt-4">
+      <section className="flex flex-wrap mt-4 gap-2">
         {pedalList && pedalList.map((item) => {
           const isOwned = myPedalIdList?.includes(item.id)
           return (
             <div
-              className="w-[200px] h-[200px] relative group p-4 hover:bg-gray-200 rounded-2xl gap-8  overflow-hidden"
+              className="bg-linear-(--pedal-bg) shadow-pedal-selector w-[200px] h-[200px] relative group p-4 hover:bg-gray-200 rounded-2xl gap-8 overflow-hidden"
               key={`${item.name}:${item.id}`}>
               {isOwned && (
-                <div className="content-['Owned'] absolute bg-amber-500 rotate-[-45deg] text-white text-xs px-12 pt-12 pb-2 top-[-28px] left-[-60px] ">Owned</div>
+                <div className="shadow-pedal-selector font-bold absolute bg-amber-500 rotate-[-45deg] text-white text-xs px-12 pt-12 pb-2 top-[-28px] left-[-60px] ">Owned</div>
               )}
               <div className="hidden absolute top-0 left-0 w-full h-full bg-black/50 rounded-2xl group-hover:flex items-center justify-center gap-4">
                 {!isOwned && (
