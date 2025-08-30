65502de930e9eb0081d9695f5b68c01a33016e37
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-08-30 07:13:19 -0700

    feat: myGearBoards and myGearPedals root pages load now, just need wrapper components for them

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import { MyBoard } from './routes/MyGear/Boards/MyBoard'
 import { ProtectedRoute } from './routes/ProtectedRoute'
 import { ROUTES } from './constants/routes'
 import { useGetSession } from './queryHooks/auth/useGetSession'
+import { MyGearPedals } from './routes/MyGear/MyGearPedals'
+import { MyGearBoards } from './routes/MyGear/MyGearBoards'
 
 function App() {
   useGetSession()
@@ -47,6 +49,18 @@ function App() {
               path={ROUTES.MY_GEAR} element={
                 <ProtectedRoute>
                   <MyGear />
+                </ProtectedRoute>
+              } />
+            <Route
+              path={ROUTES.MY_GEAR_PEDALS} element={
+                <ProtectedRoute>
+                  <MyGearPedals />
+                </ProtectedRoute>
+              } />
+            <Route
+              path={ROUTES.MY_GEAR_BOARDS} element={
+                <ProtectedRoute>
+                  <MyGearBoards />
                 </ProtectedRoute>
               } />
             <Route
